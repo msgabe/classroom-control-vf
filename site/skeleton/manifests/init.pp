8ddc1f42e12b43df463e3cc76a1736d5dc36afd9
msgabe <mgabriel@diligent.com>
2016-01-12 13:36:17 -0500

    add module

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,13 +1,17 @@
 class skeleton {
   
   file { '/etc/skel':
-  ensure => directory,
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0755',
   }
   file { '/etc/skel/.bashrc':
-  ensure => file,
-  source =>
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644', #written by owner and read by everyone else
+    source => 'puppet:///modules/skeleton/bashrc',
   }
-  file { '/site/skeleton/files/bashrc':
-  ensure => file,
-
+  
 }
