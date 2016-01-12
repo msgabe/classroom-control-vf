4af6561e0baec8fe49094546dc34754894e3d6e0
msgabe <mgabriel@diligent.com>
2016-01-12 17:54:52 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -6,11 +6,16 @@ class nginx {
   file{'/var/www':
     ensure => directory,
   }
-  file {'index.html':
-    source => 
+  file {'/etc/sysconfig/index.html':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    source => 'puppet:///modules/nginx/index.html',
+    require => Package['nginx'],
   }
   service {'nginx':
     ensure => running,
-    
+    subscribe => File['/etc/sysconfig/index.html'],
   }
 |
