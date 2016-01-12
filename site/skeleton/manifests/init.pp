9069add5e23812fed21398c16587276c81b062b2
msgabe <mgabriel@diligent.com>
2016-01-12 13:42:47 -0500

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -6,7 +6,7 @@ class skeleton {
     group => 'root',
     mode => '0755',
   }
-  file { '/etc/skeleton/.bashrc':
+  file { '/etc/skel/.bashrc':
     ensure => file,
     owner => 'root',
     group => 'root',
