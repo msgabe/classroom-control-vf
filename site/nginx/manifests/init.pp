0d34e88e7d5da0c282c82d1befc19cbfe865d212
msgabe <mgabriel@diligent.com>
2016-01-13 13:10:29 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -3,39 +3,30 @@ class nginx {
   package{'nginx':
     ensure => present,
   }
+  file { '/var/www':
+    ensure => directory,
+  }
   file {'/var/www/index.html':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
     source => 'puppet:///modules/nginx/index.html',
     require => Package['nginx'],
   }
-  file { '/var/www':
-    ensure => directory,
-    owner => 'root',
-    group => 'root',
-    mode => '0755',
-  }
-  file { '/etc/nginx/nginx.conf':
+  file { '/etc/nginx/nginx.conf':    
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
     require => Package['nginx'],
     source => 'puppet:///modules/nginx/nginx.conf',
+    notify => Service['nginx'],
   }
   file { '/etc/nginx/conf.d/default.conf':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
     source => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
+    notify => Service['nginx'],
   }
   service {'nginx':
     ensure => running,
     enable => true,
-    subscribe => [File['/var/www/index.html'],File['/etc/nginx/conf.d/default.conf']]
+    
+    #subscribe => [File['/var/www/index.html'],File['/etc/nginx/conf.d/default.conf']]
   }
 }
